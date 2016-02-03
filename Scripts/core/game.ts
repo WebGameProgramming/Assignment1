8b384bc314e237136101b6ac93c5598b46893520
aakash <oza.aakash@gmail.com>
2016-02-02 22:50:36 -0500

    modified game.ts

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -12,7 +12,7 @@ var scene: number;
 
 // files names are intro,lefcave,rightcave...
 var intro: scenes.Intro;
-var leftCave2: scenes.LeftCave;
+var leftCave: scenes.leftcave2;
 var rightCave: scenes.RightCave;
 
 function init(): void {
@@ -82,9 +82,9 @@ function changeScene(): void {
         case config.Scene.LEFT_CAVE:
             // show the PLAY scene
             stage.removeAllChildren();
-            leftCave2 = new scenes.LeftCave();       // scenes.LeftCave is variable which points to leftcave.ts file.
+            leftCave = new scenes.leftcave2();       // scenes.LeftCave is variable which points to leftcave.ts file.
             //ASSIGNING THE leftcave.ts file 
-            currentScene = leftCave2;
+            currentScene = leftCave;
             console.log("Starting LEFT_CAVE Scene");
             break;
      //----------------------------------------------------------------------------------       
